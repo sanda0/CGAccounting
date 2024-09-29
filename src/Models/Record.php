9d095d46355b4e489397011e724e06ffa75f82e6
sanda0 <sandakelum.dev@gmail.com>
2024-09-29 17:32:31 +0530

    Refactor Record model to remove unused code

--- src/Models/Record.php
+++ src/Models/Record.php
@@ -23,85 +23,85 @@ class Record extends Model
     ];
 
 
-    protected static function boot()
-    {
-        parent::boot();
+    // protected static function boot()
+    // {
+    //     parent::boot();
 
-        static::created(function ($record) {
-            $fromAccount = Account::find($record->from_account);
-            if ($fromAccount->parent_id != null) {
-                // $parentAccount = Account::find($fromAccount->parent_id);
-                $parentRecord = Record::where('from_account', $fromAccount->parent_id)->orderBy('id', 'desc')->first();
-                if ($parentRecord) {
-                    $parentLastBalance = $parentRecord->balance;
-                } else {
-                    $parentLastBalance = 0;
-                }
-                if ($record->debit > 0) {
-                    if ($fromAccount->type == "assets" || $fromAccount->type == "expenses") {
-                        $new_balance = $parentLastBalance + $record->debit;
+    //     // static::created(function ($record) {
+    //     //     $fromAccount = Account::find($record->from_account);
+    //     //     if ($fromAccount->parent_id != null) {
+    //     //         // $parentAccount = Account::find($fromAccount->parent_id);
+    //     //         $parentRecord = Record::where('from_account', $fromAccount->parent_id)->orderBy('id', 'desc')->first();
+    //     //         if ($parentRecord) {
+    //     //             $parentLastBalance = $parentRecord->balance;
+    //     //         } else {
+    //     //             $parentLastBalance = 0;
+    //     //         }
+    //     //         if ($record->debit > 0) {
+    //     //             if ($fromAccount->type == "assets" || $fromAccount->type == "expenses") {
+    //     //                 $new_balance = $parentLastBalance + $record->debit;
 
-                        $new_entry = new Record();
-                        $new_entry->from_account = $fromAccount->parent_id;
-                        $new_entry->to_account = $fromAccount->parent_id;
-                        $new_entry->debit = $record->debit;
-                        $new_entry->credit = 0;
-                        $new_entry->balance = $new_balance;
-                        $new_entry->ref_id = 0;
-                        $new_entry->ref_type = "system";
-                        $new_entry->description = "Balance update";
-                        $new_entry->save();
-                    } elseif (in_array($fromAccount->type, ['liabilities', 'equity', 'income'])) {
-                        $new_balance = $parentLastBalance - $record->debit;
+    //     //                 $new_entry = new Record();
+    //     //                 $new_entry->from_account = $fromAccount->parent_id;
+    //     //                 $new_entry->to_account = $fromAccount->parent_id;
+    //     //                 $new_entry->debit = $record->debit;
+    //     //                 $new_entry->credit = 0;
+    //     //                 $new_entry->balance = $new_balance;
+    //     //                 $new_entry->ref_id = 0;
+    //     //                 $new_entry->ref_type = "system";
+    //     //                 $new_entry->description = "Balance update";
+    //     //                 $new_entry->save();
+    //     //             } elseif (in_array($fromAccount->type, ['liabilities', 'equity', 'income'])) {
+    //     //                 $new_balance = $parentLastBalance - $record->debit;
 
-                        $new_entry = new Record();
-                        $new_entry->from_account = $fromAccount->parent_id;
-                        $new_entry->to_account = $fromAccount->parent_id;
-                        $new_entry->debit = $record->debit;
-                        $new_entry->credit = 0;
-                        $new_entry->balance = $new_balance;
-                        $new_entry->ref_id = 0;
-                        $new_entry->ref_type = "system";
-                        $new_entry->description = "Balance update";
-                        $new_entry->save();
+    //     //                 $new_entry = new Record();
+    //     //                 $new_entry->from_account = $fromAccount->parent_id;
+    //     //                 $new_entry->to_account = $fromAccount->parent_id;
+    //     //                 $new_entry->debit = $record->debit;
+    //     //                 $new_entry->credit = 0;
+    //     //                 $new_entry->balance = $new_balance;
+    //     //                 $new_entry->ref_id = 0;
+    //     //                 $new_entry->ref_type = "system";
+    //     //                 $new_entry->description = "Balance update";
+    //     //                 $new_entry->save();
 
-                        return true;
-                    }
-                } elseif ($record->credit > 0) {
-                    if ($fromAccount->type == "assets" || $fromAccount->type == "expenses") {
-                        $new_balance = $parentLastBalance - $record->credit;
+    //     //                 return true;
+    //     //             }
+    //     //         } elseif ($record->credit > 0) {
+    //     //             if ($fromAccount->type == "assets" || $fromAccount->type == "expenses") {
+    //     //                 $new_balance = $parentLastBalance - $record->credit;
 
-                        $new_entry = new Record();
-                        $new_entry->from_account = $fromAccount->parent_id;
-                        $new_entry->to_account = $fromAccount->parent_id;
-                        $new_entry->debit = 0;
-                        $new_entry->credit = $record->credit;
-                        $new_entry->balance = $new_balance;
-                        $new_entry->ref_id = 0;
-                        $new_entry->ref_type = "system";
-                        $new_entry->description = "Balance update";
-                        $new_entry->save();
-                    } elseif (in_array($fromAccount->type, ['liabilities', 'equity', 'income'])) {
-                        $new_balance = $parentLastBalance + $record->credit;
+    //     //                 $new_entry = new Record();
+    //     //                 $new_entry->from_account = $fromAccount->parent_id;
+    //     //                 $new_entry->to_account = $fromAccount->parent_id;
+    //     //                 $new_entry->debit = 0;
+    //     //                 $new_entry->credit = $record->credit;
+    //     //                 $new_entry->balance = $new_balance;
+    //     //                 $new_entry->ref_id = 0;
+    //     //                 $new_entry->ref_type = "system";
+    //     //                 $new_entry->description = "Balance update";
+    //     //                 $new_entry->save();
+    //     //             } elseif (in_array($fromAccount->type, ['liabilities', 'equity', 'income'])) {
+    //     //                 $new_balance = $parentLastBalance + $record->credit;
 
-                        $new_entry = new Record();
-                        $new_entry->from_account = $fromAccount->parent_id;
-                        $new_entry->to_account = $fromAccount->parent_id;
-                        $new_entry->debit = 0;
-                        $new_entry->credit = $record->credit;
-                        $new_entry->balance = $new_balance;
-                        $new_entry->ref_id = 0;
-                        $new_entry->ref_type = "system";
-                        $new_entry->description = "Balance update";
-                        $new_entry->save();
+    //     //                 $new_entry = new Record();
+    //     //                 $new_entry->from_account = $fromAccount->parent_id;
+    //     //                 $new_entry->to_account = $fromAccount->parent_id;
+    //     //                 $new_entry->debit = 0;
+    //     //                 $new_entry->credit = $record->credit;
+    //     //                 $new_entry->balance = $new_balance;
+    //     //                 $new_entry->ref_id = 0;
+    //     //                 $new_entry->ref_type = "system";
+    //     //                 $new_entry->description = "Balance update";
+    //     //                 $new_entry->save();
 
-                        return true;
-                    }
-                }
-            }
-            return true;
-        });
-    }
+    //     //                 return true;
+    //     //             }
+    //     //         }
+    //     //     }
+    //     //     return true;
+    //     // });
+    // }
 
 }
 
