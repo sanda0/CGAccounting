085d698367828c61cf207d8001916b327312abce
sanda0 <sandakelum.dev@gmail.com>
2024-12-03 21:29:07 +0530

    Bump version to 1.0.17 in composer.json and add migration to update 'ref_type' and 'ref_id' columns in cheques table

---
+++ src/migrations/2024_11_30_091940_create_cheques_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cheques', function (Blueprint $table) {
+            $table->string('ref_type', 25)->nullable()->change();
+            $table->string('ref_id', 25)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cheques', function (Blueprint $table) {
+            $table->string('ref_type', 10)->nullable()->change();
+            $table->string('ref_id', 10)->nullable()->change();
+        });
+    }
+};
